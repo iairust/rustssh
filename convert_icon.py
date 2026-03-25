cb9493b90676c8972c428ffcfd2cf90b8ef17a78
heihei2202 <402580954@qq.com>
2026-03-25 23:03:38 +0800

    合并远程更改，保留本地版本

--- convert_icon.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-try:
-    img = Image.open(r'e:\AiCode\Codeup\xshell.ngo100.com\src-tauri\icons\icon.png')
-    img.save(r'e:\AiCode\Codeup\xshell.ngo100.com\src-tauri\icons\icon.ico', format='ICO', sizes=[(256,256), (128,128), (64,64), (32,32), (16,16)])
-    print("Icon converted successfully")
-except Exception as e:
-    print(f"Error: {e}")
